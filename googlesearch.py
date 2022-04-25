22f58a1c6a8505f2102df7fd09a05f611513702f
M Desai <bindu.m_desai@nokia.com>
2022-04-25 14:58:12 +0530

    Initial commit

---
+++ googlesearch.py
@@ -0,0 +1,13 @@
+import playwright
+from playwright.sync_api import sync_playwright
+import allure
+
+
+@allure.title("A some test tile")
+def test_dynamic_display_name():
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    allure.dynamic.link("http://playwright.dev")
+    page.goto("http://playwright.dev")
+    allure.dynamic.description("login to playwright site")
