b471dee45b79e6bed45547be19a03ad53cf4e030
bindumadhav100 <90021984+bindumadhav100@users.noreply.github.com>
2022-04-27 19:23:23 +0530

    Update googlesearch.py

--- googlesearch.py
+++ googlesearch.py
@@ -1,6 +1,6 @@
 import playwright
 from playwright.sync_api import sync_playwright
-
+import allure
 
 
 
@@ -8,7 +8,7 @@ def test_dynamic_display_name():
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=True)
     page = browser.new_page()
-#     allure.dynamic.link("http://playwright.dev")
+    allure.dynamic.link("http://playwright.dev")
     page.goto("http://playwright.dev")
     print("test is completed")
-#     allure.dynamic.description("login to playwright site")
+    allure.dynamic.description("login to playwright site")
